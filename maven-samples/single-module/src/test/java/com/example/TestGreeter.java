d906fdbdb6ec20c01316f0defab66d4ccc5f9aee
kiran <udayete@gmail.com>
2020-01-17 08:19:16 -0500

    Test case

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -43,4 +43,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Kiran";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
  }
