68f4412ad5f9117ff3785481cddaeba709c2217b
kiran <udayete@gmail.com>
2020-01-17 08:15:26 -0500

    Modified test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -39,7 +39,7 @@ public class TestGreeter {
   }
      @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "Happy New Year Kiran";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
